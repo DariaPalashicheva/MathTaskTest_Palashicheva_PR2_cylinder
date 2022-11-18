726a668c08e49f36a478b8833f84a4362b500bea
u31504 <u31504@P31504>
2022-11-19 09:32:02 +1000

    Добавьте файлы проекта.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MathTaskTest_Palashicheva_PR2_cylinder")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MathTaskTest_Palashicheva_PR2_cylinder")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d2ff5bf9-8025-40e9-94e0-a5a111cc461e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using MathTask_Palashicheva_PR2_cylinder;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MathTaskTest_Palashicheva_PR2_cylinder
+{
+    [TestClass]
+    public class GeometryTests
+    {
+        [TestMethod]
+        public void Cylinder_returned()
+        {
+            //исходные данные
+            double r = 2;
+            double h = 4;
+            double expected = 50;
+
+            // получаем значение с помощью тестируемого метода
+            Geometry g = new Geometry();
+            double actual = g.RectangleArea(r, h);
+
+
+            // сравнеие ожидаемого результата с полученным
+            Assert.AreEqual(expected, Math.Round(actual));
+        }
+    }
+}
